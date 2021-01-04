4072e09806ca12ee8bb5df9249f63e9b92c264b7
Ashar889 <https://github.com/Ashar889>
2021-01-03 19:59:10 -1200

    Front

--- server.py
+++ server.py
@@ -1,49 +1,43 @@
-import mysql.connector
-from flask import Flask, redirect, url_for, request,render_template
-
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  passwd="mysql",
-  database="MyPythonDatabase"
-)
-mycursor = mydb.cursor()
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__,template_folder='template')
 
 @app.route('/')
 def hello_name():
    return render_template('index.html')
 
-@app.route('/adddoctor',methods = ['POST', 'GET'])
+@app.route('/adddoctor')
 def adddoctor():
-   if request.method == 'POST': ##check if there is post data
-      name = request.form['name']
-      department = request.form['department']
-      id = request.form['id']
-      print(name,department,id)
-      sql = "INSERT INTO Doctors (name,department, id) VALUES (%s, %s, %s)"
-      val = (name,department,id)
-      mycursor.execute(sql, val)
-      mydb.commit() 
-      return render_template('index.html')
-   else:
-      return render_template('adddoctor.html')
+   return render_template('adddoctor.html')
 
-@app.route('/viewdoctor',methods = ['POST', 'GET'])
+@app.route('/viewdoctor')
 def viewdoctor():
-   if request.method == 'POST':
-      return render_template('index.html')
-   else:
-      mycursor.execute("SELECT * FROM Doctors")
-      row_headers=[x[0] for x in mycursor.description] #this will extract row headers
-      myresult = mycursor.fetchall()
-      data={
-         'message':"data retrieved",
-         'rec':myresult,
-         'header':row_headers
-      }
-      return render_template('viewdoctor.html',data=data)
+   return render_template('viewdoctor.html')
+
+@app.route('/addpatient')
+def addpatient():
+   return render_template('addpatient.html')
+
+@app.route('/viewpatient')
+def viewpatient():
+   return render_template('viewpatient.html')
+
+@app.route('/addnurse')
+def addnurse():
+   return render_template('addnurse.html')
+
+@app.route('/viewnurse')
+def viewnurse():
+   return render_template('viewnurse.html')
+
+@app.route('/addsession')
+def addsession():
+   return render_template('addsession.html')
+
+@app.route('/viewsession')
+def viewsession():
+   return render_template('viewsession.html')
 
 if __name__ == '__main__':
    app.run()
-  
+
+   
