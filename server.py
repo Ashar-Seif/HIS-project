313159f63405402d436f2a50901c89d7431bb1e2
Ashar889 <https://github.com/Ashar889>
2021-01-15 13:08:07 -1200

    Final1000

--- server.py
+++ server.py
@@ -1,3 +1,10 @@
+from __future__ import print_function
+from apiclient import discovery
+from httplib2 import Http
+from oauth2client import file, client, tools
+from google.oauth2 import service_account
+import googleapiclient.discovery
+#from pprint import pprint
 import mysql.connector
 from flask import Flask, redirect, url_for, render_template, request, session
 import re
@@ -10,9 +17,25 @@ mydb = mysql.connector.connect(
    database="Hemodialysis"
   
 )
+#Calendar 
+
+SCOPES = ['https://www.googleapis.com/auth/sqlservice.admin']
+SERVICE_ACCOUNT_FILE = 'service.json'
+credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+#delegated_credentials = credentials.with_subject('ashar.zanqour99@eng-st.cu.edu.eg')
+sqlAdmin= googleapiclient.discovery.build('calendar', 'v3', credentials=credentials)
+SCOPES = 'https://www.googleapis.com/auth/calendar'
+store = file.Storage('storage.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+GCAL = discovery.build('calendar', 'v3', http=creds.authorize(Http()))
+#Calendar 
 
 mycursor = mydb.cursor(buffered=True)
-mycursor.execute("CREATE TABLE IF NOT EXISTS Doctors(Dcode VARCHAR (255)  NOT NULL PRIMARY KEY,password VARCHAR(255) UNIQUE NOT NULL , Dname VARCHAR(255),Mname VARCHAR(255),Lname VARCHAR(255),phone INT(50),mail VARCHAR(255) UNIQUE,Birth_date Date,Doctor_ID INT(100) UNIQUE,syndicate_number INT (100) UNIQUE,salary INT(50),gender VARCHAR(255),address text,job_rank VARCHAR(255),access_level int DEFAULT 2,image LONGBLOB)")
+mycursor.execute("CREATE TABLE IF NOT EXISTS Doctors(Dcode VARCHAR (255)  NOT NULL PRIMARY KEY,password VARCHAR(255) UNIQUE NOT NULL , Dname VARCHAR(255),Mname VARCHAR(255),Lname VARCHAR(255),phone INT(50),mail VARCHAR(255) UNIQUE,Birth_date Date,Doctor_ID INT(100) UNIQUE,syndicate_number INT (100) UNIQUE,salary INT(50),gender VARCHAR(255),address text,job_rank VARCHAR(255),access_level int DEFAULT 2,image LONGBLOB,calendarid VARCHAR (600) UNIQUE )")
 mycursor.execute("CREATE TABLE IF NOT EXISTS nurses (Ncode VARCHAR (255) NOT NULL PRIMARY KEY,password VARCHAR(255) UNIQUE NOT NULL ,Nname VARCHAR(255),Mname VARCHAR(255),Lname VARCHAR(255),phone INT(50),mail VARCHAR(255)UNIQUE,Birth_date Date,Nurse_ID INT(100)UNIQUE,syndicate_number INT (100) UNIQUE,salary INT(50),gender VARCHAR(255),address text,access_level int DEFAULT 3,image LONGBLOB )")
 mycursor.execute("CREATE TABLE IF NOT EXISTS patients(Pcode VARCHAR (255) NOT NULL PRIMARY KEY,password VARCHAR(255) UNIQUE NOT NULL ,Pname VARCHAR(255),Mname VARCHAR(255),Lname VARCHAR(255),Numofsessions int(11),Daysofsessions text,Patient_ID INT(100)UNIQUE,phone INT(14),mail VARCHAR(255)UNIQUE,age INT(11),gender VARCHAR(255),address text,Dry_weight INT (11),Described_drugs text,access_level int DEFAULT 4,SupD VARCHAR (255),FOREIGN KEY (SupD) REFERENCES doctors(Dcode))")
 mycursor.execute("CREATE TABLE IF NOT EXISTS sessions (Scode VARCHAR (255) NOT NULL PRIMARY KEY,Date Date,used_device VARCHAR(255),price INT(11),record_by VARCHAR(255),after_weight INT (11),duration INT(11),taken_drugs text,complications text, dealing_with_complications text,comments text,P_code VARCHAR (255),D_code VARCHAR (255),N_code VARCHAR (255) ,FOREIGN KEY(P_code) REFERENCES patients(Pcode),FOREIGN KEY(D_code) REFERENCES doctors(Dcode),FOREIGN KEY(N_code) REFERENCES nurses(Ncode))")
@@ -24,8 +47,8 @@ app.secret_key = 'team13'
 
 @app.route('/',methods =  ['POST', 'GET'])
 def hello_name():
-       mycursor = mydb.cursor()
-   if request.method == "POST": 
+    mycursor = mydb.cursor()
+    if request.method == "POST": 
       name = request.form['name']
       email = request.form['email']
       subject= request.form['subject']
@@ -36,7 +59,7 @@ def hello_name():
       mydb.commit()  
       mycursor.close()
       return render_template("index.html")
-   else:
+    else:
       return render_template("index.html")
 def hello_name():
     # Check if user is loggedin
@@ -47,19 +70,27 @@ def hello_name():
     # User is not loggedin redirect to login page
     return render_template('index.html')
 
+
+# Start of view contact 
 @app.route('/viewcontact')
 def viewcontact():
    mycursor.execute("SELECT * FROM contact")
    row_headers=[x[0] for x in mycursor.description] 
    myresult = mycursor.fetchall()
    return render_template('viewcontact.html',contactData = myresult)
+#End of view contact 
 
-   @app.route('/deletecontact/<string:id>',methods=['GET','POST'])
+# Start of delete contact 
+@app.route('/deletecontact/<string:id>',methods=['GET','POST'])
 def deletecontact(id):
    mycursor = mydb.cursor()
-   mycursor.execute("DELETE FROM contact WHERE ID= %s", [id])
+   mycursor.execute("DELETE FROM contact WHERE name= %s", [id])
    mydb.commit()
-   return render_template('viewcontact.html')
+   mycursor.execute("SELECT * FROM contact")
+   row_headers=[x[0] for x in mycursor.description] 
+   myresult = mycursor.fetchall()
+   return render_template('viewcontact.html' ,contactData = myresult)
+#End of delete contact 
 
 #START OF ADD DOCTOR **** http://127.0.0.1:5000/adddoctor
 #START OF Dashboard
@@ -107,10 +138,6 @@ def dashboard():
      Sumofprices=Sum[0][0]
 #sessions
      mycursor.execute("SELECT Scode FROM sessions ")
-  
-     myresult = mycursor.fetchall()
-     Sumofprices=myresult
-     mycursor.execute("SELECT Scode FROM sessions ")
      myresult = mycursor.fetchall()
      Numberofsessions=len(myresult)
      L1=[]
@@ -146,9 +173,22 @@ def adddoctor():
       Syndicate_number= request.form['Syndicate_number']
       address= request.form['address']
       Job_rank= request.form['Job_rank']
-    
-      sql = "INSERT INTO doctors ( Dcode,password,Dname,Mname,Lname,phone,mail,Birth_date,Doctor_ID,Syndicate_number,salary,gender,address,Job_rank) VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s,%s,%s, %s,%s,%s)"
-      val = (Dcode, password ,Fname,Mname,Lname,phone,mail,BD,Doctor_ID,Syndicate_number,Salary,gender,address,Job_rank)
+      calendar = {
+      'summary': 'MY SESIONS',
+      'timeZone': 'Africa/Cairo'
+      }
+      created_calendar = GCAL.calendars().insert(body=calendar).execute()
+      Calendar_ID = created_calendar['id']
+      rule = {
+        'scope': {
+            'type' : 'user',
+            'value': mail,
+        },
+        'role': 'reader'
+      }
+      created_rule = GCAL.acl().insert(calendarId=Calendar_ID, body=rule).execute()
+      sql = "INSERT INTO doctors ( Dcode,password,Dname,Mname,Lname,phone,mail,Birth_date,Doctor_ID,Syndicate_number,salary,gender,address,Job_rank,calendarid ) VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s,%s,%s, %s,%s,%s,%s)"
+      val = (Dcode, password ,Fname,Mname,Lname,phone,mail,BD,Doctor_ID,Syndicate_number,Salary,gender,address,Job_rank,Calendar_ID)
       mycursor.execute(sql, val)
       mydb.commit() 
       return render_template('index.html')
@@ -172,8 +212,13 @@ def deletedoctor(id):
    mycursor = mydb.cursor()
    mycursor.execute("DELETE FROM doctors WHERE Dcode = %s", [id])
    mydb.commit()
-   return render_template('viewdoctor.html')
-#END OF DELETE DOCTOR 
+   mycursor.execute("SELECT * FROM Doctors")
+   row_headers=[x[0] for x in mycursor.description] 
+   myresult = mycursor.fetchall()
+   return render_template('viewdoctor.html',DoctorsData = myresult)
+#END OF DELETE DOCTOR
+
+
 
 #START OF Doctor profile
 @app.route('/doctorprofile')
@@ -214,6 +259,7 @@ def addpatient():
     return render_template('addpatient.html')
 #END OF ADD PATIENT 
 
+
 #START OF VIEW PATIENT ***** http://127.0.0.1:5000/viewpatient
 @app.route('/viewpatient')
     #@app.route("/upload",methods=["post"])
@@ -224,19 +270,18 @@ def viewpatient():
    return render_template('viewpatient.html',patientsData = myresult)
 #END OF VIEW PATIENT 
 
+
 #START OF DELETE patient
 @app.route('/deletepatient/<string:id>',methods=['GET','POST'])
 def deletepatient(id):
    mycursor = mydb.cursor()
    mycursor.execute("DELETE FROM patients WHERE Pcode = %s", [id])
    mydb.commit()
-   return render_template('viewpatient.html')
-#END OF DELETE patient 
-
-
-#def upload():
- #   file = request.files["inputfile"]
-  #  return file.filename
+   mycursor.execute("SELECT * FROM patients")
+   row_headers =[x[0] for x in mycursor.description] #this will extract row headers
+   myresult = mycursor.fetchall()
+   return render_template('viewpatient.html',patientsData = myresult)
+#END OF DELETE patient
 
 #START OF ADD NURSE **** http://127.0.0.1:5000/addnurse 
 @app.route('/addnurse', methods=["GET","POST"])
@@ -275,14 +320,18 @@ def viewnurse():
 #END OF VIEW NURSE
 
 
-#START OF DELETE patient
+#START OF DELETE nurse
 @app.route('/deletenurse/<string:id>',methods=['GET','POST'])
 def deletenurse(id):
    mycursor = mydb.cursor()
    mycursor.execute("DELETE FROM nurses WHERE Ncode = %s", [id])
    mydb.commit()
-   return render_template('viewnurse.html')
+   mycursor.execute("SELECT * FROM nurses")
+   row_headers =[x[0] for x in mycursor.description] #this will extract row headers
+   myresult = mycursor.fetchall()
+   return render_template('viewnurse.html',nursesData=myresult)
 #END OF DELETE nurse
+
 
 
 #START OF ADD sessions ***** http://127.0.0.1:5000/addsession
@@ -290,7 +339,7 @@ def deletenurse(id):
 def addsession(): 
    if request.method == 'POST': ##check if there is post data
       Scode=request.form['Scode']
-      Dcode=request.form['D_code']
+      D_code=request.form['D_code']
       Ncode=request.form['N_code']
       Pcode=request.form['P_code']
       Date= request.form['Date']
@@ -304,9 +353,30 @@ def addsession():
       dealing_with_complications= request.form['dealing_with_complications']
       comments= request.form['comments']
       sql = "INSERT INTO sessions (Scode,D_code,P_code,N_code,Date,used_device,price,record_by,after_weight,duration,taken_drugs,complications,dealing_with_complications,comments) VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s,%s,%s,%s, %s, %s )"
-      val = (Scode,Dcode,Pcode,Ncode,Date,used_device,Price,record_by,after_weight,duration,taken_drugs,complications,dealing_with_complications,comments)
+      val = (Scode,D_code,Pcode,Ncode,Date,used_device,Price,record_by,after_weight,duration,taken_drugs,complications,dealing_with_complications,comments)
       mycursor.execute(sql,val)
       mydb.commit() 
+      Time=Scode
+      Am=Time[-1]
+      AM=int(Am)
+      pm=Time[-2]
+      PM=int(pm)
+      T=PM+AM
+      D=int(duration)
+      Duration=T+D
+      mycursor.execute("SELECT calendarid FROM Doctors WHERE Dcode = %s", [D_code])
+      ID= mycursor.fetchone()
+      id=ID[0]
+      calendar = GCAL.calendars().get(calendarId='primary').execute()
+      GMT_OFF = '-07:00' 
+      event = {
+      'summary': 'New session',
+      "description": 'Hemodialysis_Departement',
+      "start": {"dateTime": '{}T{:d}:00:00'.format(Date,T), "timeZone": 'Africa/Cairo'}, 
+      "end": {"dateTime":'{}T{:d}:00:00'.format(Date,Duration), "timeZone": 'Africa/Cairo'},
+     }
+     
+      e = GCAL.events().insert(calendarId=id,sendNotifications=True, body=event).execute()
       return render_template('index.html')
    else:
       return render_template('addsession.html')
@@ -325,10 +395,13 @@ def viewsession():
 #START OF DELETE session
 @app.route('/deletesession/<string:id>',methods=['GET','POST'])
 def deletesession(id):
-   mycursor = mydb.cursor()
-   mycursor.execute("DELETE FROM sessions WHERE Scode = %s", [id])
-   mydb.commit()
-   return render_template('viewsession.html')
+    mycursor = mydb.cursor()
+    mycursor.execute("DELETE FROM sessions WHERE Scode = %s", [id])
+    mydb.commit()
+    mycursor.execute("SELECT * FROM sessions")
+    row_headers=[x[0] for x in mycursor.description] 
+    myresult = mycursor.fetchall()
+    return render_template('viewsession.html',sessionsData = myresult)
 #END OF DELETE session
 
 
@@ -450,7 +523,6 @@ def register():
             #mycursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s, % s)', (fullname, username, password, email))
             sql = 'INSERT INTO accounts (id, fullname, username, password, email,access_level) VALUES (NULL, %s, %s, %s, %s, %s)'
             val = (fullname, username, password, email,access_level)
-
             mycursor.execute(sql, val)
             mydb.commit() 
             msg = 'You have successfully registered !'
@@ -480,10 +552,8 @@ def profile():
         # Show the profile page with account info
         return render_template('profile.html', account=account)
     # User is not loggedin redirect to login page
-    return redirect(url_for('hello_name'))
-
-
+    return redirect(url_for('hello_name')) 
 if __name__ == '__main__':
-   app.run()
-   #app.run(debug=True)
+   #app.run()
+   app.run(debug=True)
    
